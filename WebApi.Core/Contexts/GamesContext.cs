11b33ab5a95a2ccf0cb59616e9fb3ded736b80f8
David Donovan <David.Donovan@us.sogeti.com>
2019-09-12 00:02:45 -0400

    Added Games Model, Context, and Controller.  Added db creation to Program.cs

---
+++ WebApi.Core/Contexts/GamesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Core.Models;
+
+namespace WebApi.Core.Contexts
+{
+    public class GamesContext : DbContext
+    {
+        public GamesContext (DbContextOptions<GamesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Game> Game { get; set; }
+    }
+}
--- WebApi.Core/Program.cs
+++ WebApi.Core/Program.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WebApi.Core.Contexts;
 
 namespace WebApi.Core
 {
@@ -14,8 +16,33 @@ namespace WebApi.Core
     {
         public static void Main(string[] args)
         {
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<GamesContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+
+            host.Run();
+        }
+
+        /*
+        public static void Main(string[] args)
+        {
             CreateWebHostBuilder(args).Build().Run();
         }
+        */
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
